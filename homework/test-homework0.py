b8e07112c4bff497557d41ccdebec187fc639a2d
Juergen Jung <jjung@towson.edu>
2015-02-04 08:19:34 -0500

    test 2

--- homework/test-homework0.py
+++ homework/test-homework0.py
@@ -1,8 +1,9 @@
-# Start writing your code into this script file 
+# Start writing your code into this script file
 
 # When you are done, follow the instructions in the readme.md file.
 # It tells you how to upload the paper onto GitHub.
 
 x = 3
 y = 7
+print('result is x+y', x+y)
 print('result is x+y', x+y)
